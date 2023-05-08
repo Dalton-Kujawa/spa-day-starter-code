d22e5c15efa762d912f41c9210f77ce169983596
Dalton-Kujawa <daltonkujawa@yahoo.com>
2023-05-08 17:53:38 -0500

    Changed to bootstrap for the CSS, had to pause to finish at a later date.

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,19 +1,32 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.*;
+import java.util.Objects;
+
 public class User {
+
+    @Size(min = 5, max = 15)
+    @NotEmpty(message = "Username is required")
     private String username;
+    @Email
     private String email;
+    @NotEmpty(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
     private String password;
+    @Size(min = 6, message = "Password must be at least 6 characters long")
+    @NotNull(message = "Password do not match")
+    private String verify;
 
     public User() {
 
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String verify) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verify = verify;
     }
 
     public String getUsername() {
@@ -38,5 +51,22 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
     }
+
+    public String getVerify() {
+        return verify;
+    }
+
+    public void setVerify(String verify) {
+        this.verify = verify;
+        checkPassword();
+    }
+
+    public void checkPassword() {
+        if (password != null && verify != null && !password.equals(verify)) {
+            setVerify(null);
+        }
+    }
+
 }
